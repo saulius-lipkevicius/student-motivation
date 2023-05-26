e71105bcb285301b26afdabcecc10335b1190f53
saulius <sauliuslipkevicius@gmail.com>
2023-05-26 21:25:21 +0300

    Updates

---
+++ precache-manifest.7003977a585686cb0f963e7f49f46265.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c9e5146d0e22121a782",
+    "url": "/student-motivation/static/css/main.b6f7e1b5.chunk.css"
+  },
+  {
+    "revision": "9c9e5146d0e22121a782",
+    "url": "/student-motivation/static/js/main.31df231b.chunk.js"
+  },
+  {
+    "revision": "1c0c739516904d9851ac",
+    "url": "/student-motivation/static/js/runtime~main.2efd9474.js"
+  },
+  {
+    "revision": "3f80c64afe55d1443c2b",
+    "url": "/student-motivation/static/js/2.a758709f.chunk.js"
+  },
+  {
+    "revision": "2b2b7da40e9fe37746226683d444215a",
+    "url": "/student-motivation/static/media/getFetch.2b2b7da4.cjs"
+  },
+  {
+    "revision": "e08cb42576fff6a0052360dc459a3865",
+    "url": "/student-motivation/static/media/homegood.e08cb425.png"
+  },
+  {
+    "revision": "8d64291e47f4810f5acb7c824c9667c2",
+    "url": "/student-motivation/static/media/logo2.8d64291e.png"
+  },
+  {
+    "revision": "b8443796f6188c3f08acaa547cd81638",
+    "url": "/student-motivation/static/media/world.b8443796.png"
+  },
+  {
+    "revision": "0394ee0e3fb0596bd67ad3864b822688",
+    "url": "/student-motivation/static/media/spinner.0394ee0e.gif"
+  },
+  {
+    "revision": "3b3ad3e30d6661fb1824993fabdc7d79",
+    "url": "/student-motivation/static/media/about-1.3b3ad3e3.jpg"
+  },
+  {
+    "revision": "b381c2abd2972024a6a7e3d882208d9b",
+    "url": "/student-motivation/static/media/Gilroy-Bold.b381c2ab.ttf"
+  },
+  {
+    "revision": "2754c1b1a0923e2b58bdf7000363ee83",
+    "url": "/student-motivation/index.html"
+  }
+];
